648910b6253639356fda91e2b8bdb263de9fcea4
Kara Thrash <dvdonato@gmail.com>
2017-07-05 14:23:09 -0400

    added express

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || '3000'
+app.get('/', function (request, response) {
+  response.send('Hello Viking!')
+})
+
+app.listen(port, function () {
+  // This function is run when the app starts up.
+  console.log('Kemst þó hægt fari.')
+})
+
+console.log("what");
